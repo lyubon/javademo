42ff967e9d0ad10a2ee8279dc9e47f3c5a66a624
andrey <andrey.smirnov@mail.ru>
2021-07-03 21:01:39 +0300

    maven project

---
+++ src/main/java/com/lyubon/gui/App.java
@@ -0,0 +1,12 @@
+package com.lyubon.gui;
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        AppFrame appFrame = new AppFrame();
+
+        appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        appFrame.setVisible(true);
+    }
+}
---
+++ src/main/java/com/lyubon/gui/AppFrame.java
@@ -0,0 +1,9 @@
+package com.lyubon.gui;
+
+import javax.swing.*;
+
+public class AppFrame extends JFrame {
+    public AppFrame() {
+        setSize(300, 200);
+    }
+}
